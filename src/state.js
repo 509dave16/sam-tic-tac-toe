e68cab1cd7591e84a8c09bbfbca88aa63ece2cc0
509Dave16 <davidfall@rocketmail.com>
2016-04-18 10:02:30 -0700

    Control State: Added 'ready' resolver which is based on the gameType not being set. May change later.

--- src/state.js
+++ src/state.js
@@ -1,3 +1,5 @@
 const state = {};
 //TODO Attach pure functions to 'state' Object that resolve whether a particular Control State is true or false
+state.ready = model => !model.gameType;
+
 export default state;
