31408afb2b3bd4c39fa01f241ee6491919cd5f1d
509Dave16 <davidfall@rocketmail.com>
2016-04-18 10:19:37 -0700

    Control State: Added 'joinAsGuest' resolver which is based on the gameType being set to 'Host Game'. May change later as we would like to use enums where possible. union-types may be a package we will use for that purpose.

--- src/state.js
+++ src/state.js
@@ -2,4 +2,5 @@ const state = {};
 //TODO Attach pure functions to 'state' Object that resolve whether a particular Control State is true or false
 state.ready = model => !model.gameType;
 state.waitForGuest = model => model.gameType === 'Host Game';
+state.joinAsGuest = model => model.gameType === 'Join Game';
 export default state;
