cf98a5a9493b590b4ac68cea6062aa2b68a73ef2
509Dave16 <davidfall@rocketmail.com>
2016-04-19 08:41:47 -0700

    Control State: Adjusted 'finished' resolution since 'finished' property was added to the model.

--- src/state.js
+++ src/state.js
@@ -4,5 +4,5 @@ state.ready = model => !model.gameType;
 state.waitForGuest = model => model.gameType === 'Host Game';
 state.joinAsGuest = model => model.gameType === 'Join Game';
 state.inGame = model => !model.turn && model.movesTaken !== Math.pow(model.size, 2);
-state.finished = model => model.movesTaken === Math.pow(model.size, 2);
+state.finished = model => (model.movesTaken === Math.pow(model.size, 2) || model.finished);
 export default state;
