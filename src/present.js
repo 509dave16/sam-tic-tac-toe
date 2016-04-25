860e7f0e16ebcc8a4195f66ceb8a45c35fcf9b84
509Dave16 <davidfall@rocketmail.com>
2016-04-25 10:33:26 -0700

    Present: Put in Guard to prevent GameType from being changed multiple times.

--- src/present.js
+++ src/present.js
@@ -2,6 +2,9 @@
 const present = (dataset, model) => {
   return dispatch => {
     //TODO Create control flow structure for accepting proposed Model mutations based on current Control State and Model Values
+    if(dataset.type === 'SET_GAMETYPE' && model.gameType) {
+      return;
+    }
     dispatch(dataset);
   }
 };
