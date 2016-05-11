e7b252daecf6b056476ff3ed8bfebf2bfbfdf1cb
509Dave16 <davidfall@rocketmail.com>
2016-05-11 14:20:41 -0700

    Present now uses the action types defined in the action types file instead of string literals.

--- src/present.js
+++ src/present.js
@@ -1,12 +1,12 @@
+import actionTypes from './action-types';
 
 const present = (dataset, model) => {
   return dispatch => {
-    //TODO Create control flow structure for accepting proposed Model mutations based on current Control State and Model Values
-    if(dataset.type === 'SET_GAME_TYPE' && model.gameType) {
+    if(dataset.type === actionTypes.SET_GAME_TYPE && model.gameType) {
       return;
     }
 
-    if(dataset.type === 'INITIATE_MARK_GRID' &&
+    if(dataset.type === actionTypes.INITIATE_MARK_GRID &&
       ((model.gameType !== 'Local Game' && model.player !== model.turn)
       || (model.grid.cells[dataset.payload.move] !== ''))
       && !model.grid.finished
