5537edfd6c987072fca0a64baa4943d1462aeca7
509Dave16 <davidfall@rocketmail.com>
2016-05-31 18:30:41 -0700

    Updated to use latest tic-tac-toe-engine package which is upgraded to Firebase 3.0.3.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import initEngine from 'tic-tac-toe-engine';
-const engine = initEngine("https://tic-tac-toe-redux-sam.firebaseio.com");
+
+const config = {
+  apiKey: "AIzaSyDw4ZYqDnT9cR9P4dwH4YQJcBsiczULtOo",
+  authDomain: "tic-tac-toe-redux-sam.firebaseapp.com",
+  databaseURL: "https://tic-tac-toe-redux-sam.firebaseio.com",
+  storageBucket: "tic-tac-toe-redux-sam.appspot.com"
+};
+
+const engine = initEngine(config);
 
 import Game from './components/Game';
 
